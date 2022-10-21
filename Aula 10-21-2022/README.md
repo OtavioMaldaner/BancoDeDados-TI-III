393f25c4bdaa8883b94cb08967b5c74a226a6652
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-10-21 11:10:44 -0300

    upload

---
+++ Aula 10-21-2022/README.md
@@ -0,0 +1,10 @@
+# Exercícios
+Banco de dados: EMPRESA
+1. Fazer uma consulta SQL que retorne o nome e a idade dos funcionários baseada na
+data de nascimento.
+2. Fazer uma consulta SQL que retorne o nome do departamento onde existam funcionários
+do sexo feminino. Embora possa ser feito de forma mais simples, você deve utilizar EXISTS para
+exercitar seu uso. 
+3. Fazer uma consulta SQL que retorne o nome e o sexo do funcionário. Porém, ao invés de
+retornar ‘F’ e ‘M’ no sexo você deve retornar ‘Feminino’ e ‘Masculino’. Para isso, nesta questão
+você deve utilizar o CASE. 
---
+++ Aula 10-21-2022/atividade.sql
@@ -0,0 +1,17 @@
+-- 1. Fazer uma consulta SQL que retorne o nome e a idade dos funcionários baseada na
+-- data de nascimento.
+SELECT funcionario.Pnome, FLOOR(DATEDIFF(NOW(), funcionario.Datanasc) / 365) idade FROM funcionario;
+-- 2. Fazer uma consulta SQL que retorne o nome do departamento onde existam funcionários
+-- do sexo feminino. Embora possa ser feito de forma mais simples, você deve utilizar EXISTS para
+-- exercitar seu uso. 
+SELECT DISTINCT departamento.Dnome FROM departamento WHERE EXISTS(SELECT funcionario.Sexo FROM funcionario WHERE funcionario.Sexo = 'F' AND funcionario.Dnr = departamento.Dnumero);
+-- 3. Fazer uma consulta SQL que retorne o nome e o sexo do funcionário. Porém, ao invés de
+-- retornar ‘F’ e ‘M’ no sexo você deve retornar ‘Feminino’ e ‘Masculino’. Para isso, nesta questão
+-- você deve utilizar o CASE. 
+SELECT funcionario.Pnome, 
+CASE funcionario.Sexo
+	WHEN 'F' THEN 'Feminino'
+    WHEN 'M' THEN 'Masculino'
+    ELSE 'Não binário'
+    END 'Sexo'
+FROM funcionario;
